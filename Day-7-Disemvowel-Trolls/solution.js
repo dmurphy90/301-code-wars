fe9b12bdc89da7440a4df0eda0ae06c72c1f01f6
Dean <dean.murphy90@gmail.com>
2017-12-06 09:04:21 -0800

    added whiteboarding picture

--- Day-7-Disemvowel-Trolls/solution.js
+++ Day-7-Disemvowel-Trolls/solution.js
@@ -1,12 +1,5 @@
 'use strict';
 
 function disemvowel(str) {
-  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-  let finalPost = "";
-  for (i = 0; i <= str.length; i++) {
-    if (vowels.indexOf(str.charAt(i)) === -1) {
-      finalPost = finalPost + str.charAt(i);
-    }
-  }
-  return finalPost;
+  return str.replace(/[aeiou]/gi, '');
 }
